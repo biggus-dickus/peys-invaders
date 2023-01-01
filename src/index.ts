e8e787ed746421070cafdc6ed7fbd82d1883714b
Max Maslenko <lynchnost@gmail.com>
2023-01-01 20:15:46 +0200

    Attempt2

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import {DOMElem} from './config'
-import {game} from './game'
-import render from './main'
-
-if (DOMElem.START_BTN) {
-  DOMElem.START_BTN.addEventListener('click', () => game.start(render))
-}
